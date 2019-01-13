a6dae3cbd90e8e2747239056c95cbc98817ddca6
Jessica Woolard <jessicawoolard@Jessicas-MacBook-Pro.local>
2019-01-13 15:15:22 -0500

    python added to switch blogs

--- app.py
+++ app.py
@@ -14,23 +14,36 @@ def index():
     post_file.close()
 
     # 2. Create a new list that will hold all the html for your blog posts
+    blog_posts = []
 
     # 3. Open the csv file and read it using the CSV library. This will give you a list of rows.
-    # See: https://docs.python.org/3/library/csv.html#csv.DictReader
+        # See: https://docs.python.org/3/library/csv.html#csv.DictReader
+    with open("data.csv", newline="") as cvsfile:
+        reader = csv.DictReader(cvsfile)
 
-    # 4. Loop over each row in the CSV. Each row is a blog post.
+        # 4. Loop over each row in the CSV. Each row is a blog post.
+        for row in reader:
+            current_post_html = post_html
+            # 4. Loop over each row in the CSV. Each row is a blog post.
+            current_post_html = current_post_html.replace("{{category}}", row['category'])
+            current_post_html = current_post_html.replace("{{title}}", row['title'])
+            current_post_html = current_post_html.replace("{{body}}", row['body'])
+            current_post_html = current_post_html.replace("{{author}}", row['author'])
+            current_post_html = current_post_html.replace("{{date}}", row['date'])
+            current_post_html = current_post_html.replace("{{image}}", row['image'])
 
-    # 5. Take post_html and replace {{title}} {{body}} {{author}} with the data in each blog post csv row
 
     # 6. Add the post_html to the new list you created above.
 
-    # 7. Join all the items in your new list together into a single string. Name this string "blog_post_html".
+            blog_posts.append(current_post_html)
 
+    # 7. Join all the items in your new list together into a single string. Name this string "blog_post_html".
+    blog_posts_html = "".join(blog_posts)
     # 8. Open the index.html file and replace {{blog_posts}} with the blog post string you just created.
     index_file = open('index.html', 'r')
     index_html = index_file.read()
 
-    index_html = index_html.replace('{{blog_posts}}', blog_post_html)
+    index_html = index_html.replace('{{blog_posts}}', blog_posts_html)
 
     index_file.close()
 
